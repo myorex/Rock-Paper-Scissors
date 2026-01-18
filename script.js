8ce952f6237db8b3cd59a886769d3e17602facc2
myomrex <myom.ortserga@gmail.com>
2026-01-18 17:49:14 +0100

    building declaration and connecting external javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+  const randomNumber = Math.random();
+
+  if (randomNumber < 0.33) {
+    return "rock";
+  } else if (randomNumber < 0.66) {
+    return "paper";
+  } else {
+    return "scissors";
+  }
+}
+
+// Test
+console.log(getComputerChoice());
+
